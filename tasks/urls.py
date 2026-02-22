b333eb00434e2f8df4f6c1a6513f5db4d31cc770
Edidiong Archibong <eddymyk13@gmail.com>
2026-02-22 15:54:10 +0100

    Fix API URLs: /api/token/ endpoints and tasks routing

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet, UserViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Task Management API!")
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
@@ -9,6 +13,6 @@ router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
